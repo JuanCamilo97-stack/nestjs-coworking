3f8a1dc103679512063715aa3607769f9a233d27
Juan Atehortua <118418932+JuanCamilo97-stack@users.noreply.github.com>
2024-06-21 13:13:28 -0500

    pendiente por crear modulo que permita tener los espacios, los demas ya funcionan correctamente, se conecta a la base de datos con exito

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,33 @@ import { Module } from '@nestjs/common';
 import { BookingModule } from './booking/booking.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MeetingModule } from './meeting/meeting.module';
 
 @Module({
-  imports: [BookingModule, RoomsModule, UsersModule, MeetingModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
+      extra: {
+        ssl: true
+      },
+    }),
+    UsersModule,  
+    BookingModule, 
+    MeetingModule,
+    RoomsModule 
+  ],
 })
 export class AppModule {}
